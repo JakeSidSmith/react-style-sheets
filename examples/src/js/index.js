5b7f0a7741d0cc7ca3af9df13285e77ea5b3f4c4
Jake 'Sid' Smith <jake@dabapps.com>
2016-09-02 14:32:26 +0100

    Vendor prefixes & nested arrays

--- examples/src/js/index.js
+++ examples/src/js/index.js
@@ -4,10 +4,16 @@
 
   var ReactStyleSheets = require('../../../lib/index');
 
+  ReactStyleSheets.setVendorPrefixes({
+    transform: ['webkit', 'moz', 'ms', 'o'],
+    transformOrigin: ['webkit', 'moz', 'ms', 'o']
+  });
+
   var bodyHtml = {
     margin: 0,
     padding: 0,
-    fontFamily: ['arail', 'helvetica', 'sans-serif']
+    fontFamily: ['arail', 'helvetica', 'sans-serif'],
+    fontSize: 14
   };
 
   ReactStyleSheets.createGlobalTagStyles({
@@ -17,12 +23,14 @@
     body: bodyHtml,
     html: bodyHtml,
     p: {
-      margin: [10, 0]
+      margin: [10, 0],
+      transform: 'rotate(45deg)',
+      transformOrigin: '0 0'
     },
     h1: {
       margin: {
-        left: 0,
-        right: 10
+        top: 10,
+        bottom: 10
       }
     }
   });
