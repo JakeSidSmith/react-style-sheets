0e549da14cec526a7b016a22fb2685eec1876524
Jake 'Sid' Smith <jake@dabapps.com>
2016-09-04 00:09:49 +0100

    Fix nested style casing

--- examples/src/js/index.js
+++ examples/src/js/index.js
@@ -2,11 +2,10 @@
 
 (function () {
 
+  require('./styles');
   var React = require('react');
   var ReactDOM = require('react-dom');
   var App = require('./components/app');
-
-  require('./styles');
 
   ReactDOM.render(<App />, document.getElementById('app'));
 
