bc67fcd8a73edfe0e6b13ba172a718413e44de4a
Jake 'Sid' Smith <jake@dabapps.com>
2016-09-06 21:42:41 +0100

    Test that a style tag is appended to the head tag

--- tests/helpers/test-setup.js
+++ tests/helpers/test-setup.js
@@ -3,40 +3,9 @@
 (function () {
 
   var chai = require('chai');
-
-  // Head tags mock
-  var headTags = [
-    {
-      appendChild: function () {}
-    }
-  ];
-
-  var styleTag = {
-    setAttribute: function () {},
-    innerHTML: ''
-  };
-
-  // Document mock
-  var doc = {
-    getElementsByTagName: function (query) {
-      if (query !== 'head') {
-        throw new Error('No mocks for getElementsByTagName with query: ' + query);
-      }
-
-      return headTags;
-    },
-    createElement: function (type) {
-      if (type !== 'style') {
-        throw new Error('No mocks for createElement with type: ' + type);
-      }
-
-      return styleTag;
-    }
-  };
-
-  // Add document to global
-  global.document = doc;
+  var sinonChai = require('sinon-chai');
 
   chai.expect();
+  chai.use(sinonChai);
 
 })();
--- tests/index.js
+++ tests/index.js
@@ -3,16 +3,54 @@
 (function () {
 
   var chai = require('chai');
+  var sinon = require('sinon');
   var expect = chai.expect;
+  var spy = sinon.spy;
 
-  var ReactStyleSheets = require('../lib/index');
+  var ReactStyleSheets;
 
-  console.log(ReactStyleSheets.something);
+  // Head tags mock
+  var headTags = [
+    {
+      appendChild: function () {}
+    }
+  ];
+
+  var styleTag = {
+    setAttribute: function () {},
+    innerHTML: ''
+  };
+
+  // Document mock
+  var doc = {
+    getElementsByTagName: function (query) {
+      if (query === 'head') {
+        return headTags;
+      }
+    },
+    createElement: function (type) {
+      if (type === 'style') {
+        return styleTag;
+      }
+    }
+  };
+
+  global.document = doc;
 
   describe('React Style Sheets', function () {
 
     it('should pass this test', function () {
-      expect(true).to.be.true;
+      var getElementsByTagNameSpy = spy(document, 'getElementsByTagName');
+      var createElementSpy = spy(document, 'createElement');
+      var setAttributeSpy = spy(styleTag, 'setAttribute');
+      var appendChildSpy = spy(headTags[0], 'appendChild');
+
+      ReactStyleSheets = require('../lib/index');
+
+      expect(getElementsByTagNameSpy).to.have.been.calledWith('head');
+      expect(createElementSpy).to.have.been.calledWith('style');
+      expect(setAttributeSpy).to.have.been.calledWith('type', 'text/css');
+      expect(appendChildSpy).to.have.been.calledWith(styleTag);
     });
 
   });
